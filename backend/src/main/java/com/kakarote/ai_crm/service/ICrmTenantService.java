78af2de7b72b1d8a833a9ad6a72c9ada1be7eca5
zhangzhiwei <1206005430@qq.com>
2026-03-12 10:35:32 +0800

    saas遗漏代码

---
+++ backend/src/main/java/com/kakarote/ai_crm/service/ICrmTenantService.java
@@ -0,0 +1,7 @@
+package com.kakarote.ai_crm.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.kakarote.ai_crm.entity.PO.CrmTenant;
+
+public interface ICrmTenantService extends IService<CrmTenant> {
+}
