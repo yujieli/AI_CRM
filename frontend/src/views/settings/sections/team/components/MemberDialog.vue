e5a90df4d0ebecc84219d64779a66f2cb3e4a0e4
young <yangyarui@5kcrm.com>
2026-03-23 14:57:13 +0800

    style: 新建角色员工样式完善

--- frontend/src/views/settings/sections/team/components/MemberDialog.vue
+++ frontend/src/views/settings/sections/team/components/MemberDialog.vue
@@ -4,30 +4,43 @@
     :title="editingMember ? '编辑员工' : '新增员工'"
     :width="isMobile ? '95%' : '650px'"
     :fullscreen="isMobile"
-    class="wk-dialog--flush wk-member-dialog"
+    class="wk-dialog--flush wk-member-dialog wk-crm-el-field-scope"
   >
     <el-form :model="memberForm" label-position="top">
       <el-row :gutter="16">
         <el-col :xs="24" :sm="12">
           <el-form-item label="姓名" required>
-            <el-input v-model="memberForm.realname" placeholder="请输入员工姓名" />
+            <el-input v-model="memberForm.realname" placeholder="请输入员工姓名" class="w-full wk-crm-el-field-input" size="large" />
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="12">
           <el-form-item label="职位">
-            <el-input v-model="memberForm.post" placeholder="如：销售经理" />
+            <el-input v-model="memberForm.post" placeholder="如：销售经理" class="w-full wk-crm-el-field-input" size="large" />
           </el-form-item>
         </el-col>
       </el-row>
       <el-row :gutter="16">
         <el-col :xs="24" :sm="12">
           <el-form-item label="用户名" :required="!editingMember">
-            <el-input v-model="memberForm.username" :disabled="!!editingMember" :placeholder="editingMember ? '' : '用于系统登录'" />
+            <el-input
+              v-model="memberForm.username"
+              :disabled="!!editingMember"
+              :placeholder="editingMember ? '' : '用于系统登录'"
+              class="w-full wk-crm-el-field-input"
+              size="large"
+            />
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="12">
           <el-form-item label="登录密码" :required="!editingMember">
-            <el-input v-model="memberForm.password" type="password" show-password :placeholder="editingMember ? '留空则不修改' : '默认密码 123456'" />
+            <el-input
+              v-model="memberForm.password"
+              type="password"
+              show-password
+              :placeholder="editingMember ? '留空则不修改' : '默认密码 123456'"
+              class="w-full wk-crm-el-field-input"
+              size="large"
+            />
           </el-form-item>
         </el-col>
       </el-row>
@@ -42,13 +55,21 @@
               clearable
               check-strictly
               :render-after-expand="false"
-              style="width: 100%"
+              class="w-full wk-crm-el-field-select"
+              size="large"
             />
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="12">
           <el-form-item label="直属上级">
-            <el-select v-model="memberForm.parentId" placeholder="无 (顶级员工)" clearable filterable style="width: 100%">
+            <el-select
+              v-model="memberForm.parentId"
+              placeholder="无 (顶级员工)"
+              clearable
+              filterable
+              class="w-full wk-crm-el-field-select"
+              size="large"
+            >
               <el-option :value="null" label="无 (顶级员工)" />
               <el-option
                 v-for="user in parentOptions"
@@ -63,12 +84,12 @@
       <el-row :gutter="16">
         <el-col :xs="24" :sm="12">
           <el-form-item label="电子邮箱">
-            <el-input v-model="memberForm.email" placeholder="example@wukong.ai" />
+            <el-input v-model="memberForm.email" placeholder="example@wukong.ai" class="w-full wk-crm-el-field-input" size="large" />
           </el-form-item>
         </el-col>
         <el-col :xs="24" :sm="12">
           <el-form-item label="手机号码">
-            <el-input v-model="memberForm.mobile" placeholder="请输入手机号" />
+            <el-input v-model="memberForm.mobile" placeholder="请输入手机号" class="w-full wk-crm-el-field-input" size="large" />
           </el-form-item>
         </el-col>
       </el-row>
