840e1039df57297ab5e97eb5949d3deedfbd2b4d
zhangzhiwei <1206005430@qq.com>
2026-04-24 16:44:31 +0800

    日程的编辑功能，以及稍微调整ai对话的样式

---
+++ backend/src/main/java/com/kakarote/ai_crm/entity/BO/ScheduleUpdateBO.java
@@ -0,0 +1,50 @@
+package com.kakarote.ai_crm.entity.BO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 日程更新参数
+ */
+@Data
+@Schema(name = "ScheduleUpdateBO", description = "日程更新参数")
+public class ScheduleUpdateBO {
+
+    @NotNull(message = "日程ID不能为空")
+    @Schema(description = "日程ID", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long scheduleId;
+
+    @NotBlank(message = "标题不能为空")
+    @Schema(description = "标题", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String title;
+
+    @Schema(description = "描述")
+    private String description;
+
+    @NotNull(message = "开始时间不能为空")
+    @Schema(description = "开始时间", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Date startTime;
+
+    @Schema(description = "结束时间")
+    private Date endTime;
+
+    @Schema(description = "类型: meeting, call, visit, other")
+    private String type;
+
+    @Schema(description = "关联客户ID")
+    private Long customerId;
+
+    @Schema(description = "联系人ID")
+    private Long contactId;
+
+    @Schema(description = "地点")
+    private String location;
+
+    @Schema(description = "参与人员工ID列表")
+    private List<Long> participantUserIds;
+}
