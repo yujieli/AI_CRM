80fdf86bc266ab689f7b1cbafd72a25f0679b25d
zhangzhiwei <1206005430@qq.com>
2026-06-15 13:26:45 +0800

    测试：更新产品服务测试

--- backend/src/test/java/com/kakarote/ai_crm/service/impl/ProductServiceImplTest.java
+++ backend/src/test/java/com/kakarote/ai_crm/service/impl/ProductServiceImplTest.java
@@ -8,7 +8,10 @@ import com.kakarote.ai_crm.entity.PO.Product;
 import com.kakarote.ai_crm.entity.PO.ProductCategory;
 import com.kakarote.ai_crm.mapper.ManageUserMapper;
 import com.kakarote.ai_crm.mapper.ProductMapper;
+import com.kakarote.ai_crm.service.FileStorageService;
+import com.kakarote.ai_crm.service.ICustomFieldService;
 import com.kakarote.ai_crm.service.IProductCategoryService;
+import com.kakarote.ai_crm.service.ISystemConfigService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -36,11 +39,27 @@ class ProductServiceImplTest {
     @Mock
     private ManageUserMapper manageUserMapper;
 
+    @Mock
+    private ISystemConfigService systemConfigService;
+
+    @Mock
+    private ICustomFieldService customFieldService;
+
+    @Mock
+    private FileStorageService fileStorageService;
+
     private ProductServiceImpl productService;
 
     @BeforeEach
     void setUp() {
-        productService = new ProductServiceImpl(productMapper, productCategoryService, manageUserMapper);
+        productService = new ProductServiceImpl(
+                productMapper,
+                productCategoryService,
+                manageUserMapper,
+                systemConfigService,
+                customFieldService,
+                fileStorageService
+        );
         setCurrentUser(1001L);
     }
 
